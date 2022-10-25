18db17ba606e1141e856ac8824a0dbb4c914c9f2
Vishma Pratim Das <bhishmadas9@gmail.com>
2022-10-25 11:32:26 +0530

    Changed seekbar changeListener code and fixed bug in calculation

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -6,10 +6,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.SeekBar
-import android.widget.Toast
 import com.example.tipcalculator.databinding.ActivityMainBinding
-
-const val TAG="Mainactivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,24 +27,23 @@ class MainActivity : AppCompatActivity() {
         //------------------------------------------VIEWBINDING ENDS HERE---------------------------
 
         // Left side
-        var tvTipPercentageLabel=reference.tvTipPercentageLabel
+        val tvTipPercentageLabel=reference.tvTipPercentageLabel
 
         // Right side
-        var etAmount=reference.etAmount
-        var seekBar=reference.seekBar
-        var tvTipAmount=reference.tvTipAmount
-        var tvTotalAmount=reference.tvTotalAmount
+        val etAmount=reference.etAmount
+        val seekBar=reference.seekBar
+        val tvTipAmount=reference.tvTipAmount
+        val tvTotalAmount=reference.tvTotalAmount
 
         // Set listener on seekbar and reflect change in tvTipPercentagelabel and tvTipAmount
         seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 tvTipPercentageLabel.text= p1.toString()+"%"
                 // Tip=% of etAmount
-                var Base: Int=Integer.parseInt(etAmount.text.toString()).toInt()
-                var percentage: Int = p1.toInt()
-                var Tip = Base*percentage*0.1
-                Log.i("MainActivity", "Base: $Base Percentage: $percentage Tip: $Tip")
-
+                var Base = etAmount.text.toString().toInt()
+                var TipPercent = p1.toInt()
+                var Tip = (Base*TipPercent*0.01).toInt()
+                Log.i("Main Activity","Base: $Base p1: $p1 Tip percentage: $TipPercent Tip: $Tip ")
                 tvTipAmount.text="$Tip"
                 tvTotalAmount.text=(Base+Tip).toString()
             }
@@ -63,7 +59,7 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(p0: Editable?) {
-                Log.i("Text Change Listener","$p0")
+                if (p0.toString()=="") return
 
                 // Take Base, tip percentage
                 val Base=p0.toString().toInt()
@@ -71,6 +67,7 @@ class MainActivity : AppCompatActivity() {
                 tvTipAmount.text=(Base*TipPercentage*0.01).toString()
                 tvTotalAmount.text=((Base*TipPercentage*0.01)+Base).toString()
                 // Update tvTipAmount and tvTotalAmount
+                Log.i("Text Change Listener","Base: $Base Tip percentage: $TipPercentage Tip: ${tvTipAmount.text}")
             }
         })
     }
